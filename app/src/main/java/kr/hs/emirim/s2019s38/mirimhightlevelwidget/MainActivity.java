b21084e018df4d22806772ddf67631421458eefa
sumin_030829 <sumin0829@kako.com>
2020-11-02 20:10:52 +0900

    Complet activity_reservation.xml

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimhightlevelwidget/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimhightlevelwidget/MainActivity.java
@@ -10,13 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton radioDate,radioTime;
-    CalendarView calender1;
+    DatePicker datePink;
     TimePicker timaPick1;
     Chronometer chrono;
     TextView textResult;
@@ -30,15 +31,17 @@ public class MainActivity extends AppCompatActivity {
         radioTime=findViewById(R.id.radio_time);
         radioDate.setOnClickListener(radioListener);
         radioTime.setOnClickListener(radioListener);
-        calender1=findViewById(R.id.calender1);
+        datePink=findViewById(R.id.date_pick);
         timaPick1=findViewById(R.id.time_pick);
         chrono = findViewById(R.id.chrono1);
         textResult=findViewById(R.id.text_result);
-        Button btnStart=findViewById(R.id.btn_start);
-        Button btnStop=findViewById(R.id.btn_stop);
-        btnStart.setOnClickListener(btnListener);
-        btnStop.setOnClickListener(btnListener);
-        calender1.setOnDateChangeListener(calendarListener);
+        chrono.setOnClickListener(chronoListener);
+        textResult.setOnLongClickListener(textListener);
+        radioDate.setVisibility(View.INVISIBLE);
+        radioTime.setVisibility(View.INVISIBLE);
+        datePink.setVisibility(View.INVISIBLE);
+        timaPick1.setVisibility(View.INVISIBLE);
+
 
     }
     View.OnClickListener radioListener=new View.OnClickListener() {
@@ -46,41 +49,43 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.radio_date:
-                    calender1.setVisibility(View.VISIBLE);
+                    datePink.setVisibility(View.VISIBLE);
                     timaPick1.setVisibility(View.INVISIBLE);
                     break;
                 case R.id.radio_time:
-                    calender1.setVisibility(View.INVISIBLE);
+                    datePink.setVisibility(View.INVISIBLE);
                     timaPick1.setVisibility(View.VISIBLE);
                     break;
             }
         }
     };
 
-    View.OnClickListener btnListener=new View.OnClickListener() {
+    View.OnClickListener chronoListener=new View.OnClickListener() {
         @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.btn_start:
-                    chrono.setBase(SystemClock.elapsedRealtime());
-                    chrono.start();
-                    chrono.setTextColor(Color.RED);
-                    break;
-                case R.id.btn_stop:
-                    chrono.stop();
-                    chrono.setTextColor(Color.BLUE);
-                    textResult.setText(dateStr+timaPick1.getCurrentHour()+"시"+timaPick1.getCurrentMinute()+"분");
-                    break;
-            }
+        public void onClick(View view) {
+            chrono.setBase(SystemClock.elapsedRealtime());
+            chrono.start();
+            chrono.setTextColor(Color.RED);
+            radioDate.setVisibility(View.VISIBLE);
+            radioTime.setVisibility(View.VISIBLE);
         }
     };
 
-    String dateStr="";
-
-    CalendarView.OnDateChangeListener calendarListener =new CalendarView.OnDateChangeListener() {
+    View.OnLongClickListener textListener=new View.OnLongClickListener() {
         @Override
-        public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-            dateStr=year+"년"+(month+1)+"월"+dayOfMonth+"일";
+        public boolean onLongClick(View view) {
+            chrono.stop();
+            chrono.setTextColor(Color.BLUE);
+            textResult.setText(datePink.getYear()+"년"+(1+datePink.getMonth())+"월" +
+                    datePink.getDayOfMonth()+"일");
+            radioDate.setVisibility(View.INVISIBLE);
+            radioTime.setVisibility(View.INVISIBLE);
+            datePink.setVisibility(View.INVISIBLE);
+            timaPick1.setVisibility(View.INVISIBLE);
+            return false;
         }
     };
+
+
+
 }
